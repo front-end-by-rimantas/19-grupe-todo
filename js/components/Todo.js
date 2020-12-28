24d2b1e90fadec0911b31a5202047a26f6db0a4b
Rimantas <belauzas@yahoo.com>
2020-12-28 14:45:25 +0200

    Edit todo item

--- js/components/Todo.js
+++ js/components/Todo.js
@@ -4,6 +4,8 @@ class Todo {
 
         this.DOM = null;
         this.taskList = [];
+
+        this.editForm = null;
     }
 
     init() {
@@ -56,8 +58,9 @@ class Todo {
     }
 
     // CRUD: update
-    updateTask() {
-
+    updateTask(itemIndex, newText) {
+        this.taskList[itemIndex].text = newText;
+        this.renderList();
     }
 
     // CRUD: delete
@@ -75,37 +78,12 @@ class Todo {
             const removeBtn = item.querySelector('.btn.remove');
 
             editBtn.addEventListener('click', () => {
-                this.initTodoItemEditing(i);
+                this.editForm.show(i);
             })
             removeBtn.addEventListener('click', () => {
                 this.deleteTask(i);
             })
         }
-    }
-
-    initTodoItemEditing(taskIndex) {
-        const task = this.taskList[taskIndex];
-
-        const lightbox = document.querySelector('.lightbox');
-        const formUpdate = lightbox.querySelector('form.update');
-        const textarea = formUpdate.querySelector('textarea');
-        const buttonCancel = formUpdate.querySelector('button.cancel');
-        const buttonUpdate = formUpdate.querySelector('button.update');
-
-        lightbox.dataset.form = 'update';
-        textarea.value = task.text;
-        lightbox.classList.add('show');
-
-        buttonCancel.addEventListener('click', e => {
-            e.preventDefault();
-            lightbox.classList.remove('show');
-        })
-        buttonUpdate.addEventListener('click', e => {
-            e.preventDefault();
-            this.taskList[taskIndex].text = textarea.value;
-            lightbox.classList.remove('show');
-            this.renderList();
-        })
     }
 }
 
--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 import { Todo } from './components/Todo.js';
+import { EditForm } from './components/EditForm.js';
 
 const addNewTask = document.querySelector('.add-new');
 const lightbox = document.querySelector('.lightbox');
@@ -12,6 +13,14 @@ const todo = new Todo({
     selector: 'main'
 });
 todo.init();
+
+const editForm = new EditForm({
+    selector: 'form.update',
+    todoObject: todo
+});
+editForm.init();
+
+todo.editForm = editForm;
 
 // add events
 addNewTask.addEventListener('click', () => {
